d7d673de2f45b95415fcd88fa351fcc2ad951e6d
Nkhobarekar <nidhikhobarekar@gmail.com>
2025-02-11 20:51:07 +0530

    k

---
+++ list.py
@@ -0,0 +1,49 @@
+
+#LIST 
+
+my_list = ['apple','kaju','chiku']
+
+my_list.append('mango')
+
+print("List after append:", my_list)
+
+#STRING UPPER
+my_string = "Nidhi Khobarekar "
+
+upper_string = my_string.upper()
+
+print("String after upper:", upper_string)
+
+#STRING REPLACE
+
+my_string = "Pooja Abhilasha Sanchita"
+
+new_string = my_string.replace ("Abhilasha", "Nidhi")
+
+print("String after replace:", new_string)
+
+#TUPLE COUNT
+
+my_tuple = (12,13,45,65,12)
+
+count_of_twos = my_tuple.count(12)
+
+print("Count of 2 in tuple:", count_of_twos)
+
+#Dictionary Get 
+
+my_dict = {'name': 'Ramesh', 'age': 23}
+
+name = my_dict.get('name')
+
+print("Value retrieved from dictionary:", name)
+
+#TUPLE ADD
+my_set = {1, 2, 3}
+
+my_set.add(4)
+
+print("Set after add:", my_set)
+
+
+ 
